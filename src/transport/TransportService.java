ba10eec47b7450fa95cf863eb9351005e79f4e30
nauka <241878@astanait.edu.kz>
2024-12-26 10:21:39 +0500

    TransportService.java

--- src/transport/TransportService.java
+++ src/transport/TransportService.java
@@ -1,4 +1,34 @@
 package transport;
 
 public class TransportService {
+    private Bus bus;
+    private Passenger passenger;
+
+    public TransportService(Bus bus, Passenger passenger) {
+        this.bus = bus;
+        this.passenger = passenger;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public void setBus(Bus bus) {
+        this.bus = bus;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public String toString() {
+        return "TransportService{" +
+                "bus=" + bus +
+                ", passenger=" + passenger +
+                '}';
+    }
 }
